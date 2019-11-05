5dfdb1a0480d220e2f29e5b4972572c75750ef35
rta3 <55838480+rta3@users.noreply.github.com>
2019-11-05 20:30:49 +0000

    Add files via upload
    Now have a dog sprite moving with the arrow keys.

--- AnimalDash.js
+++ AnimalDash.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("gameScreen");
 var ctx = canvas.getContext("2d");
 
+//Variables for sprites 
+var player;
+
 //Variables for palyer construction.
 var playerWidth = 100;
 var playerHeight = 100;
@@ -8,6 +11,7 @@ var playerPositionX = (canvas.width - playerWidth);// X and Y given same initial
 var playerPositionY = (canvas.width - playerWidth);
 var playerVelocityX = 5;
 var playerVelocityY = 5;
+
 
 //Variables for keyboard inputs.
 var rightPressed;
@@ -28,32 +32,30 @@ function keyDownHandler(event)
     }
     else if(event.keyCode == 40)
     {
-        //Down
         downPressed = true;
     }
     else if(event.keyCode == 38)
     {
-        //Up
         upPressed = true;
     }
 }
 
 function keyUphandler(event)
 {
-    if(event.keyCode == 39)
+    if(event.keyCode == 39)//39 is Right
     {
         rightPressed = false;
     }
-    else if(event.keyCode == 37)
+    else if(event.keyCode == 37)//37 is Left
     {
         leftPressed = false;
     }
-    else if(event.keyCode == 40)
+    else if(event.keyCode == 40)//40 is Down
     {
         //Down
         downPressed = false;
     }
-    else if(event.keyCode == 38)
+    else if(event.keyCode == 38)//38 is Up
     {
         //Up
         upPressed = false;
@@ -63,13 +65,38 @@ function keyUphandler(event)
 document.addEventListener('keydown',keyDownHandler,false);
 document.addEventListener('keyup', keyUphandler,false);
 
+class SpriteConstuctor 
+{
+    constructor(width, height, color, imagePosX, imagePosY, type) 
+    {
+        this.type = type;
+        if (type == "image") 
+        {
+            this.image = new Image();
+            this.image.src = color;
+        }
+        this.width = width;
+        this.height = height;
+        this.imagePosX = playerPositionX;
+        this.imagePosY = playerPositionY;
+ 
+        ctx.drawImage(this.image, this.imagePosX, this.imagePosY, this.width, this.height);
+
+
+    }
+}
+
+
+
 function drawPlayer()// Draw function fraws the player.
 {
+    
     ctx.beginPath();
     ctx.rect(playerPositionX, playerPositionY ,playerWidth,playerHeight);
-    ctx.fillStyle = "red";
-    ctx.fill();
+    //ctx.fillStyle = "red";
+    //ctx.fill();
     ctx.closePath();
+  
 }
 
 function draw() 
@@ -93,9 +120,9 @@ function draw()
     {
         playerPositionY += playerVelocityY;
     }
-
+    
     drawPlayer();
-
+    player = new SpriteConstuctor(67,20,"DogSprite.png", playerPositionY, playerVelocityY, "image");
     requestAnimationFrame(draw);
 }
 
